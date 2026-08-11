4154cec74d924e8411400e3f627b829a6c0d8d33
anannyak1233 <anannyak1233@gmail.com>
2026-08-11 20:45:59 +0530

    Remove misplaced website folder

--- adamya-birthday-website/serve.py
+++
@@ -1,7 +0,0 @@
-from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
-from pathlib import Path
-import os
-
-os.chdir(Path(__file__).resolve().parent)
-print("Open http://localhost:8000")
-ThreadingHTTPServer(("127.0.0.1", 8000), SimpleHTTPRequestHandler).serve_forever()
